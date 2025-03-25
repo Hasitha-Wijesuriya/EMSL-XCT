e7e8bf3627c7db84c027893cbf461d4e15c7c726
Hasitha-Wijesuriya <hasitha@x1403c7s7b0n0h0.chn.perlmutter.nersc.gov>
2025-03-25 09:33:05 -0700

    nersc settings

---
+++ backend/.ipynb_checkpoints/job_mbir-checkpoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --account emsl61599                    # charged account
+#SBATCH --gres=gpu:2
+#SBATCH --time 120                           # 2 hour 0 minute time limit
+#SBATCH --nodes 1                              # 1 nodes
+#SBATCH --job-name mbirjax                 # job name in queue (``squeue``)
+#SBATCH --error my_job_name-%j.err             # stderr file with job_name-job_id.err
+#SBATCH --output my_job_name-%j.out            # stdout file
+#SBATCH --mail-user=hasitha.wijesuriya@pnnl.gov  # email user
+#SBATCH --mail-type END                        # when job ends
+
+source ~/venv/anaconda3/etc/profile.d/conda.sh                                 # removes the default module set
+conda deactivate
+conda deactivate
+conda activate emsl-xct
+
+srun python /home/wije370/EMSL-XCT/backend/mbir_script.py
